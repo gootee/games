488aba20a83f329cac8506300052864e767e0ff3
John Gootee <gootee1565@gmail.com>
2019-11-21 11:41:47 -0800

    adjacent mine display working

--- public/javascripts/minesweeper.js
+++ public/javascripts/minesweeper.js
@@ -1,8 +1,13 @@
+// import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from "constants";
+
 // Beginner – 9 * 9 Board and 10 Mines
 // Intermediate – 16 * 16 Board and 40 Mines
 // Advanced – 24 * 24 Board and 99 Mines
 
-let dimension = 16, mineCount = 0, cells;
+let dimension = 16,
+  mineCount = 0, 
+  cells
+
 
 function setBoardParams() {
   const cells = document.querySelector(".minesweeper_cells")
@@ -32,45 +37,120 @@ function setBoardParams() {
   }
 }
 
-function loadBoard() {
-  setBoardParams();
-  let htmlString = '',
-    mineIndices = [], 
-    cellCount = dimension * dimension;
+// function convertIndexToRowAndColumn(index) {
+//   const cellPosition = {}
 
-  while (mineIndices.length < mineCount) {
-    const testIndex = Math.floor(Math.random() * cellCount)
-    if (mineIndices.indexOf(testIndex) === -1) {
-      mineIndices.push(testIndex);
+//   cellPosition.row = Math.ceil((index + 1)/dimension);
+//   cellPosition.col = ((index + 1) % dimension) > 0 ? ((index + 1) % 9) : 9;
+  
+//   return cellPosition;
+// }
+
+function convertRowAndColumnToIndex(row, col) {
+  return (row - 1) * dimension + col - 1
+}
+
+function countAdjacentMines(index, mineMap) {
+  let adjacentBombCount = 0;
+  const indexCellColumnNum = (index > 0 && index % dimension === 0) ? dimension : index % dimension + 1;
+  const indexCellRowNum = index === 0 ? 1 : Math.ceil(index/dimension);
+  const leftSearchColumnNum = indexCellColumnNum === 1 ? 1 : indexCellColumnNum - 1;
+  const rightSearchColumnNum = indexCellColumnNum === dimension ? dimension : indexCellColumnNum + 1;
+  const topSearchRowNum = indexCellRowNum === 1 ? 1 : indexCellRowNum - 1;
+  const bottomSearchRowNum = indexCellRowNum === dimension ? dimension : indexCellRowNum + 1;
+
+  for (let row = topSearchRowNum; row <= bottomSearchRowNum; row++) {
+    for (let col = leftSearchColumnNum; col <= rightSearchColumnNum; col++) {
+      const thisIndex = convertRowAndColumnToIndex(row, col)
+      if (mineMap[thisIndex]) {
+        adjacentBombCount ++;
+      }
+    }
+  }
+
+  return adjacentBombCount;
+}
+
+// function getAdjacentMineCount() {
+
+// }
+
+function loadBoard() {
+  const cellCount = dimension * dimension;
+  let mineMap = Array(cellCount).fill(false),
+    htmlString = '',
+    minesCreated = 0
+
+  setBoardParams();    
+
+  while (minesCreated < mineCount) {
+    const testIndex = Math.floor(Math.random() * cellCount) 
+    if (!mineMap[testIndex]) {
+      mineMap[testIndex] = true;
+      minesCreated ++
     }
   }
 
   for (let i = 0; i < cellCount; i++) {
-
-    //bomb 
+    // debugger;
+    const hasBomb = mineMap[i];
+    const adjacentBombCount = countAdjacentMines(i, mineMap)
+    const cellDisplay = getCellDisplay(hasBomb, adjacentBombCount)
     const classString =
-    "class='minesweeper_cell" +
-    (mineIndices.indexOf(i) === -1 ? '' : ' hasBomb') +
-    "'"
+      "class='minesweeper_cell" +
+      " adjacent_mine_count" +    //keep adjacentMineCount at classList[1]
+      adjacentBombCount.toString() +
+      (hasBomb ? ' hasBomb' : '') +
+      "'"
 
     htmlString += 
-    "<div id=" + 
-    i.toString() + 
-    " " +
-    classString +
-    ">" + 
-    "<p class='cell_display'>&#128163;</p>" +
-    "</div>"
+      "<div id=" + 
+      i.toString() + 
+      " " +
+      classString +
+      ">" + 
+      "<p class='cell_display'>" +
+      cellDisplay.display +
+      "</p>" +
+      "</div>"
   }
 
   document.querySelector(".minesweeper_cells").insertAdjacentHTML("afterbegin", htmlString)
 }
 
-// function revealBoard() {
-//   cells.forEach(cell => {
-//     cell.
-//   })
-// }
+function getCellDisplay(hasBomb, adjacentBombCount) {
+  if (hasBomb) {
+    return { 
+      displayString: "&#128163;",
+      color: ""
+    }
+  } else {
+    let assignedColour
+    switch (adjacentBombCount) {
+      case 0: {
+        assignedColour = "green"
+        break;        
+      }
+      case 1: {
+        assignedColour = "red"
+        break;
+      }
+      case 2: {
+        assignedColour = "blue"
+        break;        
+      }
+      default: {
+        assignedColour = "brown"
+        break;          
+      }
+    }
+    return {
+      displayString: adjacentBombCount.toString(),
+      color: assignedColour      
+    }
+  }
+
+}
 
 function revealCell(cell) {
   cell.classList.add("revealed_cell")
@@ -84,19 +164,12 @@ function clickCell(event) {
   }
 }
 
-// function clickDisplay(event) {
-//   debugger
-//   const cell = event.target.parentElement
-//   testCell(cell)
-// }
-
 function load() {
   // debugger
   loadBoard();
   cells = document.querySelectorAll(".minesweeper_cell")
   cells.forEach(cell => {
     cell.addEventListener("click", clickCell)
-    // cell.querySelector(".cell_display").addEventListener("click", clickDisplay)
   })
 }
 
