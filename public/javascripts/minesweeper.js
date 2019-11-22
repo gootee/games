72d7f38521ede06d355128558baf3d4cf4464934
John Gootee <gootee1565@gmail.com>
2019-11-22 11:13:51 -0800

    zero click cascade working

--- public/javascripts/minesweeper.js
+++ public/javascripts/minesweeper.js
@@ -1,5 +1,3 @@
-// import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from "constants";
-
 // Beginner – 9 * 9 Board and 10 Mines
 // Intermediate – 16 * 16 Board and 40 Mines
 // Advanced – 24 * 24 Board and 99 Mines
@@ -7,7 +5,6 @@
 let dimension = 16,
   mineCount = 0, 
   cells
-
 
 function setBoardParams() {
   const cells = document.querySelector(".minesweeper_cells")
@@ -50,10 +47,10 @@ function convertRowAndColumnToIndex(row, col) {
   return (row - 1) * dimension + col - 1
 }
 
-function countAdjacentMines(index, mineMap) {
-  let adjacentBombCount = 0;
-  const indexCellColumnNum = (index > 0 && index % dimension === 0) ? dimension : index % dimension + 1;
-  const indexCellRowNum = index === 0 ? 1 : Math.ceil(index/dimension);
+function getAdjacentCells(index) {
+  let adjacentCellIndices = [];
+  const indexCellColumnNum = (index > 0 && index % dimension === 0) ? 1 : index % dimension + 1;
+  const indexCellRowNum = index % dimension === 0 ? index/dimension + 1 : Math.ceil(index/dimension);
   const leftSearchColumnNum = indexCellColumnNum === 1 ? 1 : indexCellColumnNum - 1;
   const rightSearchColumnNum = indexCellColumnNum === dimension ? dimension : indexCellColumnNum + 1;
   const topSearchRowNum = indexCellRowNum === 1 ? 1 : indexCellRowNum - 1;
@@ -62,18 +59,33 @@ function countAdjacentMines(index, mineMap) {
   for (let row = topSearchRowNum; row <= bottomSearchRowNum; row++) {
     for (let col = leftSearchColumnNum; col <= rightSearchColumnNum; col++) {
       const thisIndex = convertRowAndColumnToIndex(row, col)
-      if (mineMap[thisIndex]) {
-        adjacentBombCount ++;
+      if (thisIndex != index) {
+        adjacentCellIndices.push(thisIndex);
       }
     }
   }
 
-  return adjacentBombCount;
+  return adjacentCellIndices;
 }
 
-// function getAdjacentMineCount() {
+function countAdjacentMines(index, mineMap) {
 
-// }
+
+  // if (index === 0 || index % dimension === 0) {
+  //   debugger        
+  // }
+  const adjacentCells = getAdjacentCells(index);
+  let adjacentMineCount = 0;
+
+  for (adjacentCellIndex of adjacentCells) {
+
+    if (mineMap[adjacentCellIndex]) {
+      adjacentMineCount ++;
+    }    
+  }
+
+  return adjacentMineCount;
+}
 
 function loadBoard() {
   const cellCount = dimension * dimension;
@@ -92,25 +104,28 @@ function loadBoard() {
   }
 
   for (let i = 0; i < cellCount; i++) {
-    // debugger;
+
     const hasBomb = mineMap[i];
-    const adjacentBombCount = countAdjacentMines(i, mineMap)
-    const cellDisplay = getCellDisplay(hasBomb, adjacentBombCount)
+    const adjacentMineCount = countAdjacentMines(i, mineMap)
+    const cellDisplay = getCellDisplay(hasBomb, adjacentMineCount)
     const classString =
       "class='minesweeper_cell" +
       " adjacent_mine_count" +    //keep adjacentMineCount at classList[1]
-      adjacentBombCount.toString() +
+      adjacentMineCount.toString() +
       (hasBomb ? ' hasBomb' : '') +
       "'"
-
     htmlString += 
       "<div id=" + 
       i.toString() + 
       " " +
       classString +
       ">" + 
-      "<p class='cell_display'>" +
-      cellDisplay.display +
+      "<p class='cell_display'" + 
+      "style='color:" +
+      cellDisplay.color +
+      ";'" +
+      ">" +
+      cellDisplay.displayString +
       "</p>" +
       "</div>"
   }
@@ -118,7 +133,7 @@ function loadBoard() {
   document.querySelector(".minesweeper_cells").insertAdjacentHTML("afterbegin", htmlString)
 }
 
-function getCellDisplay(hasBomb, adjacentBombCount) {
+function getCellDisplay(hasBomb, adjacentMineCount) {
   if (hasBomb) {
     return { 
       displayString: "&#128163;",
@@ -126,9 +141,9 @@ function getCellDisplay(hasBomb, adjacentBombCount) {
     }
   } else {
     let assignedColour
-    switch (adjacentBombCount) {
+    switch (adjacentMineCount) {
       case 0: {
-        assignedColour = "green"
+        assignedColour = "black"
         break;        
       }
       case 1: {
@@ -139,33 +154,94 @@ function getCellDisplay(hasBomb, adjacentBombCount) {
         assignedColour = "blue"
         break;        
       }
+      case 3: {
+        assignedColour = "green"
+        break;        
+      }
+      case 4: {
+        assignedColour = "purple"
+        break;        
+      }
+      case 5: {
+        assignedColour = "maroon"
+        break;        
+      }
+      case 6: {
+        assignedColour = "yellow"
+        break;        
+      }    
+      case 7: {
+        assignedColour = "orange"
+        break;        
+      }
+      case 8: {
+        assignedColour = "black"
+        break;        
+      }  
       default: {
         assignedColour = "brown"
         break;          
       }
     }
     return {
-      displayString: adjacentBombCount.toString(),
+      displayString: adjacentMineCount.toString(),
       color: assignedColour      
     }
   }
 
 }
 
+function convertAdjacentMineCount(classString) {
+  // adjacent_mine_count
+  const num = classString.substr(19)
+  return parseInt(num);
+}
+
 function revealCell(cell) {
   cell.classList.add("revealed_cell")
 }
 
+function revealAdjacentCells(index) {
+
+}
+
 function clickCell(event) {
+  // debugger
   const cell = event.currentTarget;
-  revealCell(cell);
   if (cell.classList.contains("hasBomb")) {
 
+  } else {
+    let cellsIndicesToReveal = [parseInt(cell.id)];
+    currentCounter = 0;
+
+    while ((cellsIndicesToReveal) && cellsIndicesToReveal.length >= currentCounter + 1) {
+      const curCellIndex = cellsIndicesToReveal[currentCounter];
+      const curCell = document.getElementById(curCellIndex.toString())
+      const adjacentMineCount = convertAdjacentMineCount(curCell.classList[1])
+
+      if (adjacentMineCount === 0) {
+        const adjacentCellIndices = getAdjacentCells(curCell.id)
+        if (adjacentCellIndices.length > 0) {
+          for (let i = 0; i < adjacentCellIndices.length; i++) {
+            const adjacentCellIndex = adjacentCellIndices[i]
+            if (!document.getElementById(adjacentCellIndex).classList.contains("revealed_cell")) {
+              if (cellsIndicesToReveal.indexOf(adjacentCellIndex) === -1) {
+                cellsIndicesToReveal.push(adjacentCellIndex)
+              }              
+            }
+          }          
+        }
+      }  
+
+      revealCell(curCell);
+      currentCounter++;
+    }
   }
 }
 
+
 function load() {
-  // debugger
+  // debugger;
   loadBoard();
   cells = document.querySelectorAll(".minesweeper_cell")
   cells.forEach(cell => {
